36eea4a0259e07c422b614f8552bdfd88f8bd58c
chand26 <iamchandanass@domain.com>
2024-12-29 13:45:59 +0530

    Initial commit with portfolio files

--- src/contents/About.js
+++ src/contents/About.js
@@ -5,13 +5,13 @@ class About extends Component {
         return (
             <div className="condiv">
             <h1 className="subtopic">About Me</h1>
-            <h4>Hey!</h4>
+            <h4> Hello!</h4>
             <h1>I'm Chandana B S</h1>
-            <h3>Full Stack Web Developer </h3>
+            <h3> Web Developer </h3>
             <br></br>
             <p>I started my journey in the world of computers from an young age,
             now I’m 22 years old, Pursuing my Master's of Computer Application in
-            Surana College(Autonomous), Bangalore. Web development is my center of interest, i always
+            Surana College (Autonomous), Bangalore. Web development is my center of interest, i always
             love the idea of cross-platform development. 
             </p>
             </div>
--- src/contents/Education.js
+++ src/contents/Education.js
@@ -5,10 +5,10 @@ render() {
 return (
 <div className="condiv">
 <h1 className="subtopic">My Education</h1>
-<Widecard title="Master of Computer Applications" where="Surana College" from="Feb 2024" to="Present"/>
-<Widecard title="Bachelor of Computer Applications" where="KLE College" from="2020" to="2023"/>
-<Widecard title="PUC" where="RNS College" from="2018" to="2020"/>
-<Widecard title="SSLC" where="VVIS School" from="2015" to="2018"/>
+<Widecard title="Master of Computer Applications" where="Surana College (Autonomous)" from="Feb 2023" to="Pursuing"/>
+<Widecard title="Bachelor of Computer Applications" where="KLE Society's Degree College" from="2020" to="2023"/>
+<Widecard title="PUC" where="RNS pre-university College" from="2018" to="2020"/>
+<Widecard title="SSLC" where="VVIS School" from="2017" to="2018"/>
 </div>
 )
 }
